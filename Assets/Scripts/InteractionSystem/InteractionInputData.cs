539504bd728436bc163f270eb4babbc534599681
Alexander Köhn <kohnalex2@gmail.com>
2020-04-26 01:15:11 +0200

    [WIP] handling player Input

---
+++ Assets/Scripts/InteractionSystem/InteractionInputData.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "InteractionInputData", menuName = "InteractionSystem/InputData")]
+public class InteractionInputData : ScriptableObject
+{
+    private bool interactClicked;
+    private bool interactReleased;
+
+    public bool InteractClicked
+    {
+        get => interactClicked;
+        set => interactClicked = value;
+    }
+
+    public bool InteractReleased
+    {
+        get => interactReleased;
+        set => interactReleased = value;
+    }
+
+    public void Reset()
+    {
+        interactClicked = false;
+        interactReleased = false;
+    }
+}
