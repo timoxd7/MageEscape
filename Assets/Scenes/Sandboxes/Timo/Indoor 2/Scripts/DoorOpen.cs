82bffdf0a2305c95c031631035d7602885b0a976
timoxd7 <timoxd7@gmx.de>
2020-04-28 23:46:04 +0200

    Addition to 8e86d7cal...

---
+++ Assets/Scenes/Sandboxes/Timo/Indoor 2/Scripts/DoorOpen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    public Animator animator;
+    public string doorOpenAnimationTrigger;
+    public string playerTag;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == playerTag)
+        {
+            animator.SetTrigger(doorOpenAnimationTrigger);
+        }
+    }
+}
---
+++ Assets/Scenes/Sandboxes/Timo/Indoor 2/Scripts/KeyInsert.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyInsert : BaseInteractable
+{
+
+    public GameObject animatedKey;
+    public Animator animatedKeyAnimator;
+    public Animator openChestAnimator;
+    public float waitAfterInsertion = 1.3f;
+    public string keyRotationAnimationTrigger = "Unlock";
+    public string chestOpenAnimationTrigger = "Open";
+
+    private Timer openTimer;
+
+    void Start()
+    {
+        animatedKey.SetActive(false);
+        openTimer = new Timer();
+    }
+
+    void Update()
+    {
+        if (openTimer.GetStarted())
+        {
+            if (openTimer.Get() > waitAfterInsertion)
+            {
+                // If key was inserted and it is longer than waitAfterInsertion, the Open animation will be triggered and this Object will be destroyed (Cleaning!!!)
+                openChestAnimator.SetTrigger(chestOpenAnimationTrigger);
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    public override void OnInteract()
+    {
+        // -> Key inserted
+        // Make animated Key visible
+        animatedKey.SetActive(true);
+
+        // Start Unlock animation and save start Time
+        animatedKeyAnimator.SetTrigger(keyRotationAnimationTrigger);
+        openTimer.Start();
+    }
+}
