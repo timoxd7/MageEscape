000a9569cacff10ff220b3b4419d6260f1a46ab5
timoxd7 <timoxd7@gmx.de>
2020-05-07 15:30:58 +0200

    3297 - Add simple Debug Text GUI

---
+++ Assets/Scripts/Helper/DebugGUIWriter.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugGUIWriter : MonoBehaviour
+{
+    public float lineSpacing = 0;
+    public float lineOffset = 10;
+    public Vector2 initPosition = new Vector2(10, 10);
+    public Vector2 textBoxSize = new Vector2(500, 20);
+
+    private List<string> debugLines;
+    private bool clearNeeded = false;
+
+    void Awake()
+    {
+        debugLines = new List<string>();
+    }
+
+    void OnGUI()
+    {
+        float currentLineOffset = 0;
+
+        // Print each Line
+        foreach(string line in debugLines)
+        {
+            if (line != null && line != "")
+            {
+                GUI.Label(new Rect(initPosition.x, initPosition.y + currentLineOffset, textBoxSize.x, textBoxSize.y), line);
+                currentLineOffset += lineSpacing + lineOffset;
+
+                Debug.Log("Writing " + line);
+            }
+        }
+
+        // Empty buffer
+        clearNeeded = true;
+    }
+
+    // Should only be called from inside a Update() method (anythinb BEFORE OnGui()!)
+    public void AddLine(string line)
+    {
+        if (clearNeeded)
+        {
+            clearNeeded = false;
+            debugLines.Clear();
+        }
+
+        debugLines.Add(line);
+    }
+}
