9b378f76afcf5b0f36d71dec621f56da0008ada3
timoxd7 <timoxd7@gmx.de>
2020-05-22 03:16:58 +0200

    3150 - Reimplemented MainMenu
    0000 - Update Folder Structure

---
+++ Assets/Scripts/LevelLoader/LevelLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Slider progressBar;
+    public Animator transitionAnimator;
+    public float animationTime;
+    public string transitionTrigger;
+    
+    public void LoadLevel(string sceneName)
+    {
+        StartCoroutine(LoadAsync(sceneName));
+    }
+
+    IEnumerator LoadAsync(string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+
+        // Wait for Animation
+        yield return new WaitForSeconds(animationTime);
+
+        // Wait for Scene
+        while (!operation.isDone)
+        {
+            progressBar.gameObject.SetActive(true);
+            progressBar.value = Mathf.Clamp01(operation.progress / 0.9f);
+
+            yield return null;
+        }
+    }
+}
