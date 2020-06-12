ef0cebf5345bf48b09bdd5b7d9445eb934411ef4
timoxd7 <timoxd7@gmx.de>
2020-06-12 16:46:46 +0200

    3798 - Add DialogSystem

---
+++ Assets/Scripts/DialogSystem/DialogButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogButton : MonoBehaviour
+{
+    public DialogMessage parentMessage;
+    public DialogOption dialogOption;
+
+    public void OnClick()
+    {
+        dialogOption.Execute(parentMessage);
+    }
+}
---
+++ Assets/Scripts/DialogSystem/DialogOption.cs
@@ -0,0 +1,56 @@
+﻿using MyBox;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DialogOption : MonoBehaviour
+{
+    public string optionName;
+    public EventType eventType;
+
+    [ConditionalField(nameof(eventType), false, EventType.UnityEvent)]
+    public UnityEvent unityEvent;
+
+    [ConditionalField(nameof(eventType), false, EventType.DialogMessage)]
+    public DialogMessage dialogMessage;
+
+
+    public void Execute(DialogMessage parentMessage)
+    {
+        if (eventType == EventType.None)
+        {
+            Debug.LogWarning("No execution context given!", this);
+            return;
+        }
+
+        parentMessage.Hide();
+
+        switch (eventType)
+        {
+            case EventType.Close:
+                // Will be closed automatically
+                break;
+
+            case EventType.UnityEvent:
+                if (unityEvent == null)
+                    Debug.LogError("No UnityEvent given!", this);
+                else
+                    unityEvent.Invoke();
+                break;
+
+            case EventType.DialogMessage:
+                if (dialogMessage == null)
+                    Debug.LogError("No dialogMessage given!", this);
+                else
+                    dialogMessage.Show(parentMessage);
+                break;
+        }
+    }
+
+    public enum EventType
+    {
+        None,
+        Close,
+        UnityEvent,
+        DialogMessage
+    }
+}
