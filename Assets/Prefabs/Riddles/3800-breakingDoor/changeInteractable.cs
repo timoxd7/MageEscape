be3f8b675cc027ca70e2999f284c61330f46d4ed
Philipp <philippguggenberger1412@gmail.com>
2020-06-16 13:48:56 +0200

    3800 - added Door Riddle

---
+++ Assets/Prefabs/Riddles/3800-breakingDoor/changeInteractable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeInteractable : MonoBehaviour
+{
+    public bool changeTo;
+    public GameObject sceneObject;
+    void change()
+    {
+        sceneObject.GetComponent<Interactable>().isInteractable = changeTo;
+        Destroy(sceneObject.GetComponent<Interactable>().gameObject.GetComponent<HighlightDetection>()); 
+        sceneObject.GetComponent<Interactable>().detectionOption = DetectionOption.ConsoleLog;
+        
+    }
+}
