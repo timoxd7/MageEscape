c08ab623ff15af37bfd1e7fe2e0ccb9dd717d914
RomanP <roman.peter@hof-university.de>
2020-06-30 15:52:10 +0200

    MainMenu and Reset working

---
+++ Assets/Scripts/LevelLoader/LevelLoaderLite.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoaderLite : MonoBehaviour
+{
+    public Animator transitionAnimator;
+    
+    public void LoadLevel(string sceneName)
+    {
+        StartCoroutine(LoadAsync(sceneName));
+    }
+
+    IEnumerator LoadAsync(string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+
+        // Wait for Scene
+        while (!operation.isDone)
+        {
+            yield return null;
+        }
+    }
+}
+
