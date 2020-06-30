b3933aa9aabbfcd17bf855146e0e5e605e143ba8
timoxd7 <timoxd7@gmx.de>
2020-06-30 15:40:24 +0200

    4058 - Add ZoneLoader

---
+++ Assets/Scripts/ZoneLoader/ZoneLoadPoint.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class ZoneLoadPoint : MonoBehaviour
+{
+    public ZoneLoader zone;
+    public ZoneLoadPointType loadPointType;
+    public string playerTag = "Player";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == playerTag)
+        {
+            if (zone == null)
+            {
+                Debug.LogError("No zone given!", this);
+                return;
+            }
+
+            switch (loadPointType)
+            {
+                case ZoneLoadPointType.None:
+                    Debug.LogError("ZoneLoadPointType not spezified!", this);
+                    break;
+
+                case ZoneLoadPointType.LoadIn:
+                    zone.ActivateZone();
+                    break;
+
+                case ZoneLoadPointType.LoadOut:
+                    zone.DeactivateZone();
+                    break;
+
+                default:
+                    Debug.LogError("ZoneLoadPointType illegal", this);
+                    break;
+            }
+        }
+    }
+
+    public enum ZoneLoadPointType : byte
+    {
+        None = 0,
+        LoadIn = 1,
+        LoadOut = 2
+    }
+}
---
+++ Assets/Scripts/ZoneLoader/ZoneLoader.cs
@@ -0,0 +1,56 @@
+﻿using MyBox;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneLoader : MonoBehaviour
+{
+    public bool loadInOnGameStart = false;
+    public List<GameObject> zoneObjects;
+
+    private bool currentLoadState = false;
+
+    private void Awake()
+    {
+        if (loadInOnGameStart)
+        {
+            Load(true);
+        } else
+        {
+            Load(false);
+        }
+    }
+
+    public void ActivateZone()
+    {
+        Load(true);
+    }
+
+    public void DeactivateZone()
+    {
+        Load(false);
+    }
+
+
+    private void Load(bool activeState)
+    {
+        if (activeState == currentLoadState)
+            return;
+
+        if (zoneObjects.IsNullOrEmpty())
+        {
+            Debug.LogError("Zone has no objects attached! Destroying this Zone!");
+            Destroy(this);
+            return;
+        }
+
+        foreach (GameObject currentObject in zoneObjects)
+        {
+            if (currentObject != null)
+            {
+                currentObject.SetActive(activeState);
+            }
+        }
+
+        currentLoadState = activeState;
+    }
+}
