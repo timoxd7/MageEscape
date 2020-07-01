dc57639087d63ec1fedcf87de0f5a9fc4c2e1a10
timoxd7 <timoxd7@gmx.de>
2020-07-01 23:19:52 +0200

    3849 - Made Outdoor Riddle a bit easier

--- Assets/Scripts/Helper/Riddles/MoveBarrel.cs
+++ Assets/Scripts/Helper/Riddles/MoveBarrel.cs
@@ -2,48 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveBarrel : BaseInteraction
+public class MoveBarrel : MonoBehaviour
 {
     public Animator barrelAnimator;
     public string moveBarrelTrigger;
     public string playerTag;
-    public HighlightDetection highlightToDisable;
-    public Interactable interactableToDisable;
 
     private bool playerInside;
 
-    public override void OnInteraction(PlayerContext context)
+    public void RequestMove()
     {
+        if (!gameObject.activeSelf || !enabled)
+            return;
+
         if (playerInside)
         {
             if (barrelAnimator != null)
             {
                 barrelAnimator.SetTrigger(moveBarrelTrigger);
-
-                if (highlightToDisable != null)
-                {
-                    highlightToDisable.enabled = false;
-                    Destroy(highlightToDisable);
-                } else
-                {
-                    Debug.LogError("No Highlight to Disable given!", this);
-                }
-
-                if (interactableToDisable != null)
-                {
-                    interactableToDisable.enabled = false;
-                    Destroy(interactableToDisable);
-                }
-                else
-                {
-                    Debug.LogError("No interactableToDisable given!", this);
-                }
             } else
             {
                 Debug.LogError("No Animator given!", this);
             }
-
-            Destroy(gameObject);
         }
     }
 
