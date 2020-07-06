0ae466917aea3c2a496223bc1eca1924833bffe5
timoxd7 <timoxd7@gmx.de>
2020-07-06 03:01:24 +0200

    0000 - Add Progress Indicator

--- Assets/Scripts/LevelLoader/LevelLoader.cs
+++ Assets/Scripts/LevelLoader/LevelLoader.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelLoader : MonoBehaviour
 {
+    public GameObject progressIndicatorCanvas;
+    public TMPro.TMP_Text progressIndicator;
+
     public void LoadLevel(string sceneName)
     {
         StartCoroutine(LoadAsync(sceneName));
@@ -13,9 +17,16 @@ public class LevelLoader : MonoBehaviour
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
 
+        progressIndicatorCanvas.SetActive(true);
+
+
         // Wait for Scene
         while (!operation.isDone)
         {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+
+            progressIndicator.text = progress * 100f + "%";
+
             yield return null;
         }
     }
