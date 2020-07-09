2efc12c13f38fb72c4c07aa4a0336c0a6e14d79d
timoxd7 <timoxd7@gmx.de>
2020-07-09 23:20:02 +0200

    0000 - Add Brightness & Mouse-Sensitivity, Many Menu Bugfix & Cleanup, Language Fix

---
+++ Assets/Scripts/Helper/EnableMouseInputOnStart.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class EnableMouseInputOnStart : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
--- Assets/Scripts/Menus/MainMenus.cs
+++ Assets/Scripts/Menus/MainMenus.cs
@@ -1,11 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 using UnityEngine.UI;
 
 public class MainMenus : MonoBehaviour
 {
+    [Header("Volume")]
     public Slider generalVolume;
     public Slider musicVolume;
     public Slider effectsVolume;
+
+    [Header("Presentation")]
+    public Slider mouseSensitivity;
+    public Slider brightness;
+    public PostProcessVolume postProcessVolume;
+
+    [Header("Application")]
+    public GameObject exitMenu;
+
+    private ColorGrading colorGrading = null;
 
     // Update is called once per frame
     void Start()
@@ -13,6 +25,15 @@ public class MainMenus : MonoBehaviour
         generalVolume.value = PauseMenu.general;
         musicVolume.value = PauseMenu.music;
         effectsVolume.value = PauseMenu.effects;
+
+        if (Application.platform == RuntimePlatform.WebGLPlayer)
+        {
+            exitMenu.SetActive(false);
+        }
+
+        mouseSensitivity.value = PauseMenu.mouseSensitivity;
+        brightness.value = PauseMenu.brightness;
+        UpdateBrightness();
     }
 
     public void UpdateGeneral()
@@ -28,5 +49,29 @@ public class MainMenus : MonoBehaviour
     public void UpdateEffects()
     {
         PauseMenu.effects = effectsVolume.value;
+    }
+
+    public void UpdateMouse()
+    {
+        PauseMenu.mouseSensitivity = mouseSensitivity.value;
+    }
+
+    public void UpdateBrightness()
+    {
+        PauseMenu.brightness = brightness.value;
+        ApplyBrightness();
+    }
+
+    private void ApplyBrightness()
+    {
+        if (colorGrading == null)
+            postProcessVolume.profile.TryGetSettings(out colorGrading);
+
+        PauseMenu.ApplyBrightness(colorGrading);
+    }
+
+    public void ExitApplication()
+    {
+        Application.Quit();
     }
 }
